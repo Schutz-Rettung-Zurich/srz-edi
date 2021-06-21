eca70e13434582d9fe3c8c9b348399f5faa3fbed
Nicola Jordan <nicola.jordan@ost.ch>
2021-06-21 14:51:45 +0200

    * formatting changed to black where files touched * updated to newer versions * used swiss overpass-api to reduce chances of overpass timeouts

--- Backend/osmchadjango/supervise/models.py
+++ Backend/osmchadjango/supervise/models.py
@@ -4,6 +4,7 @@ from django.contrib.gis.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.gis.geos import GEOSGeometry
 from django.http.request import HttpRequest
+from django.conf import settings
 
 from osmchadjango.changeset.filters import ChangesetFilter
 from osmchadjango.feature.filters import FeatureFilter
@@ -21,7 +22,7 @@ from shapely.geometry import shape
 class AreaOfInterest(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, blank=True)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     filters = JSONField()
     geometry = models.GeometryField(blank=True, null=True)
@@ -29,7 +30,7 @@ class AreaOfInterest(models.Model):
     filterStatus = JSONField(blank=True, null=True)
 
     def __str__(self):
-        return '{} by {}'.format(self.name, self.user.username)
+        return "{} by {}".format(self.name, self.user.username)
 
     def changesets(self, request=None):
         """Return the changesets that match the filters, including the geometry
@@ -46,58 +47,56 @@ class AreaOfInterest(models.Model):
 
         qs = ChangesetFilter(self.filters, request=request).qs
 
-        if 'objectTags' in self.filters and 'filterArea' in self.filters:
+        if "objectTags" in self.filters and "filterArea" in self.filters:
             newObjectTagAreaGeoJSON = self.getGeojsonFromOverpass()
             if newObjectTagAreaGeoJSON is not None:
                 self.setObjectTagArea(newObjectTagAreaGeoJSON)
 
-                qs = qs.filter(
-                    bbox__intersects=self.objectTagArea
-                )
+                qs = qs.filter(bbox__intersects=self.objectTagArea)
 
                 merged_areas = self.getMergedAreas(newObjectTagAreaGeoJSON, False)
 
-                self.objectTagArea = GEOSGeometry(str(merged_areas['geometry']))
+                self.objectTagArea = GEOSGeometry(str(merged_areas["geometry"]))
                 self.save()
             else:
-                self.filterStatus['newChangesets'] = []
+                self.filterStatus["newChangesets"] = []
                 self.save()
                 return Changeset.objects.none()
 
         if self.geometry is not None:
-            qs = qs.filter(
-                bbox__intersects=self.geometry
-            )
+            qs = qs.filter(bbox__intersects=self.geometry)
 
-        if len(Changeset.objects.all()) == qs.count():  # This assumes that not all Changesets match the filter
-            self.filterStatus['newChangesets'] = []
+        if (
+            len(Changeset.objects.all()) == qs.count()
+        ):  # This assumes that not all Changesets match the filter
+            self.filterStatus["newChangesets"] = []
             self.save()
             return Changeset.objects.none()
 
-        if 'finished' in self.filterStatus:
-            changesetIDs = self.filterStatus['finished']
+        if "finished" in self.filterStatus:
+            changesetIDs = self.filterStatus["finished"]
             qs = qs.exclude(id__in=changesetIDs)
 
-        self.filterStatus['newChangesets'] = []
+        self.filterStatus["newChangesets"] = []
         for changeset in qs:
-            if str(changeset) not in self.filterStatus.get('inProgress', []):
-                self.filterStatus['newChangesets'].append(str(changeset))
+            if str(changeset) not in self.filterStatus.get("inProgress", []):
+                self.filterStatus["newChangesets"].append(str(changeset))
 
         self.save()
 
         return qs
 
     def deleteKeysWithEmptyValues(self):
-        if 'objectTags' in self.filters and str(self.filters['objectTags']) == '':
-            self.filters.pop('objectTags', None)
-        if 'usersToTrack' in self.filters and str(self.filters['usersToTrack']) == '':
-            self.filters.pop('usersToTrack', None)
-        if 'filterMember' in self.filters and str(self.filters['filterMember']) == '':
-            self.filters.pop('filterMember', None)
-        if 'filterArea' in self.filters and str(self.filters['filterArea']) == '':
-            self.filters.pop('filterArea', None)
-        if 'buffer' in self.filters and str(self.filters['buffer']) == '':
-            self.filters.pop('buffer', None)
+        if "objectTags" in self.filters and str(self.filters["objectTags"]) == "":
+            self.filters.pop("objectTags", None)
+        if "usersToTrack" in self.filters and str(self.filters["usersToTrack"]) == "":
+            self.filters.pop("usersToTrack", None)
+        if "filterMember" in self.filters and str(self.filters["filterMember"]) == "":
+            self.filters.pop("filterMember", None)
+        if "filterArea" in self.filters and str(self.filters["filterArea"]) == "":
+            self.filters.pop("filterArea", None)
+        if "buffer" in self.filters and str(self.filters["buffer"]) == "":
+            self.filters.pop("buffer", None)
         self.save()
 
     def setObjectTagArea(self, newObjectTagAreaGeoJSON):
@@ -106,10 +105,10 @@ class AreaOfInterest(models.Model):
         else:
             merged_areas = self.getMergedAreas(newObjectTagAreaGeoJSON, False)
 
-        self.objectTagArea = GEOSGeometry(str(merged_areas['geometry']))
+        self.objectTagArea = GEOSGeometry(str(merged_areas["geometry"]))
 
     def getBuffer(self):
-        buffer = float(self.filters.get('buffer', 0))
+        buffer = float(self.filters.get("buffer", 0))
         if buffer is None or buffer == 0 or buffer < 0.1:
             buffer = 1
         return buffer / 100000
@@ -119,39 +118,46 @@ class AreaOfInterest(models.Model):
         if isWithOldGeoJson:
             oldgeojson = json.loads(self.objectTagArea.json)
             polygons.append(shape(oldgeojson))
-        for poly in newObjectTagAreaGeoJSON['features']:
-            polygons.append(shape(poly['geometry']).buffer(self.getBuffer()))
+        for poly in newObjectTagAreaGeoJSON["features"]:
+            polygons.append(shape(poly["geometry"]).buffer(self.getBuffer()))
         merged_areas = shape_to_feature(unary_union(polygons))
         return merged_areas
 
     def getGeojsonFromOverpass(self):
-        tagValue = self.filters['objectTags']
+        tagValue = self.filters["objectTags"]
         tagValues = [
-            [i.strip() for i in t.split('=')]  # remove leading and ending spaces
-            for t in tagValue.split(',')
-            if len(t.split('=')) == 2
+            [i.strip() for i in t.split("=")]  # remove leading and ending spaces
+            for t in tagValue.split(",")
+            if len(t.split("=")) == 2
         ]
 
-        areaQuery = ''
-        overPassTagQuery = ''
+        areaQuery = ""
+        overPassTagQuery = ""
 
-        areas = self.filters['filterArea']
-        splittedAreas = areas.split(',')
+        areas = self.filters["filterArea"]
+        splittedAreas = areas.split(",")
         for area in splittedAreas:
             area = area.strip()
-            searchArea = area.replace('ü', 'u').replace('ä', 'a').replace('ö', 'o')
-            areaQuery += 'area[name="{areaName}"]->.search{searchArea};\n'.format(areaName=area, searchArea=searchArea)
+            searchArea = area.replace("ü", "u").replace("ä", "a").replace("ö", "o")
+            areaQuery += 'area[name="{areaName}"]->.search{searchArea};\n'.format(
+                areaName=area, searchArea=searchArea
+            )
             for tag in tagValues:
-                overPassTagQuery += 'nwr["{key}"="{value}"](area.search{searchArea});\n'.format(key=tag[0],
-                                                                                                value=tag[1],
-                                                                                                areaName=area,
-                                                                                                searchArea=searchArea)
+                overPassTagQuery += (
+                    'nwr["{key}"="{value}"](area.search{searchArea});\n'.format(
+                        key=tag[0], value=tag[1], areaName=area, searchArea=searchArea
+                    )
+                )
 
-        overpass_url = "http://overpass-api.de/api/interpreter"
-        overpass_query = '[out:json];\n{0}(\n{1}\n);\nout body;\n>;\nout skel qt;'.format(areaQuery, overPassTagQuery)
-        response = requests.get(overpass_url, params={'data': overpass_query})
-        if response.status_code != 200 or not response.json()['elements']:
-            return None;
+        overpass_url = settings.OVERPASS_URL
+        overpass_query = (
+            "[out:json];\n{0}(\n{1}\n);\nout body;\n>;\nout skel qt;".format(
+                areaQuery, overPassTagQuery
+            )
+        )
+        response = requests.get(overpass_url, params={"data": overpass_query})
+        if response.status_code != 200 or not response.json()["elements"]:
+            return None
         newObjectTagArea = response.json()
         return json2geojson(newObjectTagArea)
 
@@ -161,17 +167,18 @@ class AreaOfInterest(models.Model):
         """
         qs = FeatureFilter(self.filters).qs
         if self.geometry is not None:
-            return qs.filter(
-                geometry__intersects=self.geometry
-            )
+            return qs.filter(geometry__intersects=self.geometry)
         else:
             return qs
 
     class Meta:
-        unique_together = ('user', 'name',)
-        ordering = ['-date']
-        verbose_name = 'Area of Interest'
-        verbose_name_plural = 'Areas of Interest'
+        unique_together = (
+            "user",
+            "name",
+        )
+        ordering = ["-date"]
+        verbose_name = "Area of Interest"
+        verbose_name_plural = "Areas of Interest"
 
 
 class BlacklistedUser(models.Model):
@@ -188,5 +195,5 @@ class BlacklistedUser(models.Model):
         super(BlacklistedUser, self).save(*args, **kwargs)
 
     class Meta:
-        unique_together = ('uid', 'added_by')
-        ordering = ['-date']
+        unique_together = ("uid", "added_by")
+        ordering = ["-date"]
